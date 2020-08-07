b97a7e232855b4541dadc87c00af3ffc66f8e997
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-08-07 18:50:43 -0400

    added google sign in

--- GoogleAuth+Calendar/GoogleAuth+Calendar/AppDelegate.swift
+++ GoogleAuth+Calendar/GoogleAuth+Calendar/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+         // Initialize sign-in
+        GIDSignIn.sharedInstance().clientID = KeyChain.clientId
+         GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
@@ -32,6 +36,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    
+    /// This function is called upon the user sigining in
+    /// - Parameters:
+    ///   - signIn: Signs the user to googles servers
+    ///   - user: The current user being signed in
+    ///   - error: The error type
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+      if let error = error {
+        if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+          print("The user has not signed in before or they have since signed out.")
+        } else {
+          print("\(error.localizedDescription)")
+        }
+        return
+      }
+      // Perform any operations on signed in user here.
+      let userId = user.userID                  // For client-side use only!
+      let idToken = user.authentication.idToken // Safe to send to the server
+      let fullName = user.profile.name
+      let givenName = user.profile.givenName
+      let familyName = user.profile.familyName
+      let email = user.profile.email
+      // ...
+    }
+    
+    
+    /// A function to sign out of the app
+    /// - Parameters:
+    ///   - signIn: SignIn/Out of googles servers
+    ///   - user: The current user
+    ///   - error: The error type
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+      // Perform any operations when the user disconnects from app here.
+      // ...
+    }
 }
 
--- GoogleAuth+Calendar/GoogleAuth+Calendar/ViewController.swift
+++ GoogleAuth+Calendar/GoogleAuth+Calendar/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+
+        // Automatically sign in the user.
+        //GIDSignIn.sharedInstance()?.restorePreviousSignIn()
     }
 
 
